10edb3486fc4350d5821da8a45d8e1f45c7668fd
Naseef Nusky <naseefnusky09@gmail.com>
2025-10-25 11:47:42 +0530

    Add ScrollToTop component for automatic page scrolling on navigation

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import ScrollToTop from './components/ScrollToTop';
 import HomePage from './components/pages/Home';
 import Services from './components/pages/Service';
 import BlogPage from './components/pages/Blog';
@@ -21,6 +22,7 @@ import './App.css';
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <div className="App">
         <Header />
         <Routes>
